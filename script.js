8317e08fcc95ef44569c2bb91df1d3ca76ed1a20
sourSweetFriedButter0911 <doannhatminhdo@gmail.com>
2024-06-11 06:52:03 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// JavaScript code
+document.getElementById('calculateBtn').addEventListener('click', function() {
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+    var operation = document.getElementById('operation').value;
+    var result;
+
+    switch(operation) {
+        case 'add':
+            result = num1 + num2;
+            break;
+        case 'subtract':
+            result = num1 - num2;
+            break;
+        case 'multiply':
+            result = num1 * num2;
+            break;
+        case 'divide':
+            if(num2 !== 0) {
+                result = num1 / num2;
+            } else {
+                result = "Cannot divide by zero";
+            }
+            break;
+        default:
+            result = "Invalid operation";
+    }
+
+    document.getElementById('result').innerHTML = "Result: " + result;
+});
